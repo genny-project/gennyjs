12452190a30e71eecd0b4bd6035db5ed282ed4bc
matthaywardwebdesign <matt@matthayward.com.au>
2017-08-18 10:26:04 +1000

    Fixed merge conflict

--- examples/src/index.js
+++ examples/src/index.js
@@ -7,11 +7,7 @@ Logger.setLevel( LogLevel.DEBUG );
 GennyJS.init();
 
 /* Load the config from the gennyjs config server */
-<<<<<<< HEAD
 ConfigLoader.load( 'http://localhost:43669/configuration/alyson', () => {
-=======
-ConfigLoader.load( 'http://localhost:43669/configuration/auth-example', () => {
->>>>>>> df7777b4b5e64e1dc34ffae0f2cc5bdfdaba2126
   GennyJS.start();
 });
 
